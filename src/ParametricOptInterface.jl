92033ede1ae89fac17c1631937f3b408bfb4584c
Tomás Gutierrez <tomasfmgutierrez@gmail.com>
2020-06-04 18:19:38 -0300

    First commit

---
+++ src/ParametricOptInterface.jl
@@ -0,0 +1,10 @@
+module ParametricOptInterface
+
+using MathOptInterface
+
+const MOI = MathOptInterface
+const POI = ParametricOptInterface
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,3 @@
+using Test, ParametricOptInterface
+
+const POI = ParametricOptInterface
