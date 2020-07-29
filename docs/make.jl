811a06b8b90f848a1138048717df8508752a004c
Tomás Gutierrez <tomasfmgutierrez@gmail.com>
2020-07-29 10:44:21 -0300

    - deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,25 @@
-using Documenter, ParametricOptInterface
-
 push!(LOAD_PATH,"../src/")
 
-makedocs(sitename="My Documentation")
+using Documenter, ParametricOptInterface
+
+makedocs(
+    modules = [ParametricOptInterface],
+    doctest  = false,
+    clean = true,
+    # See https://github.com/JuliaDocs/Documenter.jl/issues/868
+    format = Documenter.HTML(assets = ["assets/favicon.ico"], 
+                    mathengine = Documenter.MathJax(),
+                    prettyurls = get(ENV, "CI", nothing) == "true"),
+    sitename = "ParametricOptInterface.jl",
+    authors = "Tomás Gutierrez, and contributors",
+    pages = [
+        "Home" => "index.md",
+        "manual.md",
+        "examples.md"
+        # "reference.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/tomasfmg/ParametricOptInterface.jl.git",
+)
