948a92bf815b3813ea86b6c68ecd05e5cc6dc535
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-03 19:06:32 -0700

    add test for direct mode NLP

--- test/nlp_tests.jl
+++ test/nlp_tests.jl
@@ -9,3 +9,15 @@
     @NLobjective(model, Min, x[1]^2 + x[2]^2)
     optimize!(model)
 end
+
+@testset "JuMP direct mode NLP models" begin
+    @test MOI.supports(POI.ParametricOptimizer(Ipopt.Optimizer()), MOI.NLPBlock())
+
+    optimizer = POI.ParametricOptimizer(Ipopt.Optimizer())
+    model = direct_model(optimizer)
+    @variable(model, x[i=1:2] >= 0)
+    @variable(model, y in POI.Parameter(10))
+    @constraint(model, sum(x) + y >= 0)
+    @NLobjective(model, Min, x[1]^2 + x[2]^2)
+    optimize!(model)
+end
