ef85591f549bd4784cdc2891f2b6a2affc4498f1
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-03 21:34:01 -0700

    update tests

--- test/nlp_tests.jl
+++ test/nlp_tests.jl
@@ -3,21 +3,38 @@
 
     model = Model(() -> POI.ParametricOptimizer(Ipopt.Optimizer()))
 
+    y = @variable(model, y in POI.Parameter(10))
     @variable(model, x[i=1:2] >= 0)
-    @variable(model, y in POI.Parameter(10))
-    @constraint(model, sum(x) + y >= 0)
+    @constraint(model, x[1] >= y)
+    @constraint(model, sum(x) + 2*y >= 0)
     @NLobjective(model, Min, x[1]^2 + x[2]^2)
     optimize!(model)
+    @test isapprox(JuMP.value(x[1]), JuMP.value(y))
+
+    MOI.set(model, POI.ParameterValue(), y, 20.0)
+    optimize!(model)
+    @test isapprox(JuMP.value(x[1]), 20.0)
+    @test isapprox(JuMP.value(y), 20.0)
+
+    model = Model(() -> POI.ParametricOptimizer(Ipopt.Optimizer()))
+
+    @variable(model, y in POI.Parameter(10))
+    @variable(model, x[i=1:2] >= 0)
+    @constraint(model, x[1] >= y)
+    @NLconstraint(model, sum(x[i]^2 for i in 1:2) >= 0)
+    @NLobjective(model, Min, x[1]^2 + x[2]^2)
+    optimize!(model)
+    @test isapprox(JuMP.value(x[1]), JuMP.value(y))
 end
 
 @testset "JuMP direct mode NLP models" begin
-    @test MOI.supports(POI.ParametricOptimizer(Ipopt.Optimizer()), MOI.NLPBlock())
-
-    optimizer = POI.ParametricOptimizer(Ipopt.Optimizer())
-    model = direct_model(optimizer)
-    @variable(model, x[i=1:2] >= 0)
-    @variable(model, y in POI.Parameter(10))
-    @constraint(model, sum(x) + y >= 0)
-    @NLobjective(model, Min, x[1]^2 + x[2]^2)
-    optimize!(model)
+    # Requires https://github.com/jump-dev/Ipopt.jl/issues/280
+    #@test MOI.supports(POI.ParametricOptimizer(Ipopt.Optimizer()), MOI.NLPBlock())
+    #optimizer = POI.ParametricOptimizer(Ipopt.Optimizer())
+    #model = direct_model(optimizer)
+    #@variable(model, x[i=1:2] >= 0)
+    #@variable(model, y in POI.Parameter(10))
+    #@constraint(model, sum(x) + y >= 0)
+    #@NLobjective(model, Min, x[1]^2 + x[2]^2)
+    #optimize!(model)
 end
