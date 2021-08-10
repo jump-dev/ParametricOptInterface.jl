d08d343d812674c7ab490178e6b1fe6078493f87
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-09 17:38:35 -0700

    run formatter

--- docs/make.jl
+++ docs/make.jl
@@ -2,22 +2,22 @@ using Documenter, ParametricOptInterface
 
 makedocs(
     modules = [ParametricOptInterface],
-    doctest  = false,
+    doctest = false,
     clean = true,
     # See https://github.com/JuliaDocs/Documenter.jl/issues/868
-    format = Documenter.HTML(assets = ["assets/favicon.ico"], 
-                    mathengine = Documenter.MathJax(),
-                    prettyurls = get(ENV, "CI", nothing) == "true"),
+    format = Documenter.HTML(
+        assets = ["assets/favicon.ico"],
+        mathengine = Documenter.MathJax(),
+        prettyurls = get(ENV, "CI", nothing) == "true",
+    ),
     sitename = "ParametricOptInterface.jl",
     authors = "Tomás Gutierrez, and contributors",
     pages = [
         "Home" => "index.md",
         "manual.md",
-        "example.md"
+        "example.md",
         # "reference.md"
-    ]
+    ],
 )
 
-deploydocs(
-    repo = "github.com/jump-dev/ParametricOptInterface.jl.git",
-)
+deploydocs(repo = "github.com/jump-dev/ParametricOptInterface.jl.git")
