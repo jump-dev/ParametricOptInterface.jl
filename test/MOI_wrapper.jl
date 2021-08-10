d21e8e4737f9aa26a38549deb28dd72663c277f1
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-09 18:03:16 -0700

    add first set of tests

---
+++ test/MOI_wrapper.jl
@@ -0,0 +1,25 @@
+optimizer = () -> POI.ParametricOptimizer(GLPK.Optimizer())
+const OPTIMIZER = MOI.Bridges.full_bridge_optimizer(optimizer(), Float64)
+const CONFIG = MOIT.TestConfig()
+
+@testset "Unit Tests" begin
+    MOIT.basic_constraint_tests(OPTIMIZER, CONFIG)
+    MOIT.unittest(
+        OPTIMIZER,
+        CONFIG,
+        [
+            # FIXME `NumberOfThreads` not supported
+            "number_threads",
+            # These are excluded because GLPK does not support quadratics.
+            "solve_qcp_edge_cases",
+            "solve_qp_edge_cases",
+            "delete_soc_variables",
+
+            # Tested below because the termination status is different.
+            "solve_zero_one_with_bounds_3",
+
+            # TODO(odow): not implemented.
+            "number_threads",
+        ],
+    )
+end
