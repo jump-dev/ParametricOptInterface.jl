dc8790d47f620e8f8cef7a0b103a912d552bdd47
rafabench <rafa.bench@gmail.com>
2022-07-19 13:54:02 -0300

    fix format

--- test/basic_tests.jl
+++ test/basic_tests.jl
@@ -65,13 +65,10 @@
     )
 
     @test MOI.get(optimizer, POI.ListOfPureVariableIndices()) ==
-        MOI.VariableIndex[
-            MOI.VariableIndex(1), 
-            MOI.VariableIndex(2)
-        ]
+          MOI.VariableIndex[MOI.VariableIndex(1), MOI.VariableIndex(2)]
 
     @test MOI.get(optimizer, POI.ListOfParametersIndices()) ==
-        POI.ParameterIndex[POI.ParameterIndex(1)]
+          POI.ParameterIndex[POI.ParameterIndex(1)]
 
     MOI.set(optimizer, MOI.ConstraintSet(), cy, POI.Parameter(1.0))
 
