57420424fd27b69ad584feea5125baec538b2cd8
odow <o.dowson@gmail.com>
2022-12-06 14:06:51 +1300

    Large refactoring of the tests into a functional form

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,29 +3,26 @@
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
-using Test
-using ParametricOptInterface
-using MathOptInterface
-using GLPK
-using Ipopt
-using ECOS
-using SCS
-using LinearAlgebra
 using JuMP
+using Test
+
+import GLPK
+import Ipopt
+import SCS
+
+import ParametricOptInterface
 
 const POI = ParametricOptInterface
-const MOIU = MOI.Utilities
-const MOIT = MOI.Test
 
 const ATOL = 1e-4
 
-include("production_problem_test.jl")
-include("MOI_wrapper.jl")
-include("basic_tests.jl")
-include("dual_tests.jl")
-include("quad_tests.jl")
-include("sdp_tests.jl")
-include("vector_affine_tests.jl")
-include("modifications_tests.jl")
+include("moi_tests.jl")
 include("jump_tests.jl")
-include("nlp_test.jl")
+
+for name in names(@__MODULE__; all = true)
+    if startswith("$name", "test_")
+        @testset "$(name)" begin
+            getfield(@__MODULE__, name)()
+        end
+    end
+end
