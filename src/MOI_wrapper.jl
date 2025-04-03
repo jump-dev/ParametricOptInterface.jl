049737b1f04fc5bd1531acf81d2ff16386778dcc
Guilherme Bodin <32756941+guilhermebodin@users.noreply.github.com>
2025-04-03 17:06:08 -0300

    Add validation so that parameters cannot be NaN (#173)
    * Add validation so that parameters cannot be infinite
    
    * update to is_finite
    
    * Update src/MOI_wrapper.jl
    
    * format
    
    ---------
    
    Co-authored-by: Joaquim <joaquimgarcia@psr-inc.com>
    Co-authored-by: joaquimg <joaquimdgarcia@gmail.com>

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -219,10 +219,21 @@ function MOI.supports_add_constrained_variables(
     return MOI.supports_add_constrained_variables(model.optimizer, MOI.Reals)
 end
 
+function _assert_parameter_is_finite(set::MOI.Parameter{T}) where {T}
+    if !isfinite(set.value)
+        throw(
+            AssertionError(
+                "Parameter value must be a finite number. Got $(set.value)",
+            ),
+        )
+    end
+end
+
 function MOI.add_constrained_variable(
     model::Optimizer{T},
     set::MOI.Parameter{T},
 ) where {T}
+    _assert_parameter_is_finite(set)
     _next_parameter_index!(model)
     p = MOI.VariableIndex(model.last_parameter_index_added)
     MOI.Utilities.CleverDicts.add_item(model.parameters, set.value)
@@ -563,6 +574,7 @@ function MOI.set(
     cp::MOI.ConstraintIndex{MOI.VariableIndex,MOI.Parameter{T}},
     set::MOI.Parameter{T},
 ) where {T}
+    _assert_parameter_is_finite(set)
     p = MOI.VariableIndex(cp.value)
     if !_parameter_in_model(model, p)
         error("Parameter not in the model")
