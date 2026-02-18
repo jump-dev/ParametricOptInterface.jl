e0602a0d465277122dbaa2ad017b9b725e332ee1
Oscar Dowson <odow@users.noreply.github.com>
2026-02-19 11:08:53 +1300

    [docs] add changelog (#221)

--- docs/make.jl
+++ docs/make.jl
@@ -6,6 +6,65 @@
 import Documenter
 import ParametricOptInterface
 
+# ==============================================================================
+#  Modify the release notes
+# ==============================================================================
+
+function fix_release_line(
+    line::String,
+    url::String = "https://github.com/jump-dev/ParametricOptInterface.jl",
+)
+    # (#XXXX) -> ([#XXXX](url/issue/XXXX))
+    while (m = match(r"\(\#([0-9]+)\)", line)) !== nothing
+        id = m.captures[1]
+        line = replace(line, m.match => "([#$id]($url/issues/$id))")
+    end
+    # ## Version X.Y.Z -> [Version X.Y.Z](url/releases/tag/vX.Y.Z)
+    while (m = match(r"\#\# Version ([0-9]+.[0-9]+.[0-9]+)", line)) !== nothing
+        tag = m.captures[1]
+        line = replace(
+            line,
+            m.match => "## [Version $tag]($url/releases/tag/v$tag)",
+        )
+    end
+    # ## vX.Y.Z -> [vX.Y.Z](url/releases/tag/vX.Y.Z)
+    while (m = match(r"\#\# (v[0-9]+.[0-9]+.[0-9]+)", line)) !== nothing
+        tag = m.captures[1]
+        line = replace(line, m.match => "## [$tag]($url/releases/tag/$tag)")
+    end
+    return line
+end
+
+function _fix_release_lines(changelog, release_notes, args...)
+    open(release_notes, "w") do io
+        for line in readlines(changelog; keep = true)
+            write(io, fix_release_line(line, args...))
+        end
+    end
+    return
+end
+
+_fix_release_lines(
+    joinpath(@__DIR__, "src", "changelog.md"),
+    joinpath(@__DIR__, "src", "release_notes.md"),
+)
+
+function _add_edit_url(filename, url)
+    contents = read(filename, String)
+    open(filename, "w") do io
+        write(io, "```@meta\nEditURL = \"$url\"\n```\n\n")
+        write(io, contents)
+        return
+    end
+    return
+end
+
+_add_edit_url(joinpath(@__DIR__, "src", "release_notes.md"), "changelog.md")
+
+# ==============================================================================
+#  Build and deploy the documentation
+# ==============================================================================
+
 Documenter.makedocs(;
     modules = [ParametricOptInterface],
     clean = true,
@@ -17,7 +76,12 @@ Documenter.makedocs(;
     ),
     sitename = "ParametricOptInterface.jl",
     authors = "Tomás Gutierrez, and contributors",
-    pages = ["Home" => "index.md", "background.md", "reference.md"],
+    pages = [
+        "Home" => "index.md",
+        "background.md",
+        "reference.md",
+        "release_notes.md",
+    ],
     checkdocs = :none,
 )
 
